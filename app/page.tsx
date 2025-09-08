6c8977a836542161f5d690fe353331ad8682ed10
Wahba Kamaluddin <wahba@192.168.1.9>
2025-09-08 10:32:31 +0700

    Edit errors 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import SocMedIcons from "@/app/ui/sidenav/SocMedIcons"
 
-const links = ['https://www.linkedin.com/in/wahbakamaluddin/', 'https://github.com/wahbakamaluddin', 'https://www.youtube.com/@wahbakamaluddin']
-
 export default function Page () {
  return (
     <div>
-        <h1 className="font-bold text-[30px]">print('hi')</h1>
+        <h1 className="font-bold text-[30px]">{ "print('hi')" }</h1>
         <div>
-            <p className="font-small pt-6">I'm sharing various CS stuffs that might come in handy for students, including notes from my foundation to undergraduate studies, CTF writeups, and other interesting finds. Hope it helps!</p>
+            <p className="font-small pt-6">{ "I'm sharing various CS stuffs that might come in handy for students, including notes from my foundation to undergraduate studies, CTF writeups, and other interesting finds. Hope it helps!" }</p>
             <div className="pt-6"><SocMedIcons /></div>
         </div>
 
