01376a1a05c6763837e028c5823669065fd6b3ff
Wahba Kamaluddin <wahba@wmac.local>
2025-12-29 11:13:28 +0800

    Update writings

--- app/writings/page.tsx
+++ app/writings/page.tsx
@@ -6,7 +6,7 @@ export default function Page () {
             <h1 className="font-bold text-[30px]">Writings</h1>
             </div>
             <div className="flex-1 h-full mt-6">
-            <iframe src="https://wahbakamaluddin.notion.site/ebd//25a1359f183c802ca056e7d99bf7cc84" width="100%" height="100%" frameborder="0" allowfullscreen />
+            <iframe src="https://wahbakamaluddin.notion.site/ebd//25a1359f183c802ca056e7d99bf7cc84" width="100%" height="100%" allowFullScreen />
             </div>
         </div>
         </>
