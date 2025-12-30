d3d68800ddbb74cbea31f37a6f8dc16042f7874a
Wahba Kamaluddin <wahba@wmac.local>
2025-12-30 12:59:18 +0800

    Revert "Add mobile ui adaptability"
    This reverts commit 3b3d3890decc0109fb8e1c7b099cffd33f6928b4.

--- app/ui/sidenav/sidenav.tsx
+++ app/ui/sidenav/sidenav.tsx
@@ -4,24 +4,26 @@ import NavLinks from '@/app/ui/sidenav/NavLinks';
 import SocMedIcons from '@/app/ui/sidenav/SocMedIcons';
 export default function SideNav() {
     return (
-        <nav className="flex flex-col items-center h-full px-6 py-8 md:px-10 md:py-10">
+        <div className="flex flex-col items-center h-full px-10 py-10 ">
             <div>
-                <UserCircleIcon className="h-20 w-20 md:h-28 md:w-28 text-emerald-500" strokeWidth={0.8} />
+                <UserCircleIcon className="h-28 w-28 text-emerald-500" strokeWidth={0.8} />
             </div>
-            <div className="mt-2 mb-4">
+            <div>
                 <Link 
                     href="/"
-                    className="flex flex-col items-center focus:outline-none focus:ring-2 focus:ring-emerald-500"
+                    className="flex flex-col items-center"
                 >
-                    <p className="font-medium text-[22px] md:text-[26px]">Wahba </p> 
-                    <p className="font-medium text-[22px] md:text-[26px]">Kamaluddin</p>
+                    <p className="font-medium text-[26px]">Wahba </p> 
+                    <p className="font-medium text-[26px]">Kamaluddin</p>
                 </Link>
             </div>
-            <div className="w-full">
+            <div>
                 <NavLinks />
             </div>
             <div className="h-full w-full grow"></div>
             <SocMedIcons />
-        </nav>
+        </div>
+
+        
     )
 }
