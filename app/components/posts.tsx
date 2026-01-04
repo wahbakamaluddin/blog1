123c006a40ea35012625ded66acbbae5ef344300
Wahba Kamaluddin <wahba@wmac.local>
2026-01-04 14:48:30 +0800

    Add useful resource

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -113,7 +113,7 @@ export function BlogPosts({ posts }: { posts: BlogPost[] }) {
               href={`/writing/${post.slug}`}
             >
               <div className="w-full flex flex-col md:flex-row md:items-center space-x-0 md:space-x-2">
-                <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums shrink-0">
+                <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums shrink-0">
                   {formatDate(post.metadata.publishedAt, false)}
                 </p>
                 <p className="text-neutral-900 dark:text-neutral-100 tracking-tight group-hover:underline">
