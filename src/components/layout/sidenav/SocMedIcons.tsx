a4ea90718bbe8c413b1937170c3c475db3e4753f
Wahba Kamaluddin <wahba@wmac.local>
2026-01-16 08:58:09 +0800

    Update icons

--- src/components/layout/sidenav/SocMedIcons.tsx
+++ src/components/layout/sidenav/SocMedIcons.tsx
@@ -7,9 +7,9 @@ export default function SocMedIcons () {
 
     return (
         <div className="flex flex-row gap-3">
-            <Link href={links[0]} className="hover:text-emerald-500"> <FiLinkedin size={22}/> </Link>
-            <Link href={links[1]} className="hover:text-emerald-500"> <FiGithub size={22}/> </Link>
-            <Link href={links[2]} className="hover:text-emerald-500"> <FiYoutube size={22}/> </Link>
+            <Link href={links[0]} target="_blank" rel="noopener noreferrer" className="hover:text-emerald-500"> <FiLinkedin size={22}/> </Link>
+            <Link href={links[1]} target="_blank" rel="noopener noreferrer" className="hover:text-emerald-500"> <FiGithub size={22}/> </Link>
+            <Link href={links[2]} target="_blank" rel="noopener noreferrer" className="hover:text-emerald-500"> <FiYoutube size={22}/> </Link>
             <Link href={links[3]} className="hover:text-emerald-500"> <Mail size={22}/> </Link>
         </div>
    )
