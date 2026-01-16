2e4dbe063f439440e0cc8e25edf7d307ecda8647
Wahba Kamaluddin <wahba@wmac.local>
2026-01-16 08:40:57 +0800

    Add email contact

--- src/components/layout/sidenav/SocMedIcons.tsx
+++ src/components/layout/sidenav/SocMedIcons.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
-import { LinkedinIcon, YoutubeIcon, Github } from "lucide-react"
+import { LinkedinIcon, YoutubeIcon, Github, Mail } from "lucide-react"
 
-const links = ['https://www.linkedin.com/in/wahbakamaluddin/', 'https://github.com/wahbakamaluddin', 'https://www.youtube.com/@wahbakamaluddin']
+const links = ['https://www.linkedin.com/in/wahbakamaluddin/', 'https://github.com/wahbakamaluddin', 'https://www.youtube.com/@wahbakamaluddin', 'mailto:wahbakamaluddin@yahoo.com'];
 
 export default function SocMedIcons () {
 
     return (
-        <div className="flex flex-row gap-5">
-            <Link href={links[0]} className="hover:text-emerald-500"> <LinkedinIcon /> </Link>
-            <Link href={links[1]} className="hover:text-emerald-500"> <Github /> </Link>
-            <Link href={links[2]} className="hover:text-emerald-500"> <YoutubeIcon /> </Link>
+        <div className="flex flex-row gap-3">
+            <Link href={links[0]} className="hover:text-emerald-500"> <LinkedinIcon size={22}/> </Link>
+            <Link href={links[1]} className="hover:text-emerald-500"> <Github size={22}/> </Link>
+            <Link href={links[2]} className="hover:text-emerald-500"> <YoutubeIcon size={22}/> </Link>
+            <Link href={links[3]} className="hover:text-emerald-500"> <Mail size={22}/> </Link>
         </div>
    )
 }
